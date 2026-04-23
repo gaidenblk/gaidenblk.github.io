e518a90c3dcbe9098b38688579a8b1670da8f707
Leandro Santos <gaiden_leo@hotmail.com>
2026-04-23 16:27:02 -0300

    Adicionado sistema de modal na galeria de Pixel Arts

--- pixelarts/script.js
+++ pixelarts/script.js
@@ -2,6 +2,7 @@
 	// IIFE adicionado para evitar redeclaração de variavel na SPA
 
 	let galleryContainer = document.querySelector("#galleryContainer");
+	let galleryModal = document.querySelector("#galleryModal");
 
 	const template = await carregarElemento(
 		"div",
@@ -33,6 +34,33 @@
 		clone.querySelector("img").loading = "lazy";
 		clone.querySelector(".descricao").textContent = img.descricao;
 		clone.classList.add(img.tamanho);
+
+		clone.addEventListener("click", () => {
+			if (window.innerWidth < 450) return;
+			galleryModal.style.display = "block";
+			const imgModal = document.createElement("div");
+
+			imgModal.id = "imgModal";
+			// Clona o template
+			const clone = template.cloneNode(true);
+
+			// Preenche os dados no clone
+			clone.querySelector(".titulo").textContent = img.titulo;
+			clone.querySelector("img").src = img.arquivo;
+			clone.querySelector("img").alt = img.titulo;
+			clone.querySelector("img").loading = "lazy";
+			clone.querySelector(".descricao").textContent = img.descricao;
+			clone.classList.add(img.tamanho);
+
+			imgModal.appendChild(clone);
+
+			imgModal.addEventListener("click", () => {
+				imgModal.remove();
+				galleryModal.style.display = "none";
+			});
+
+			galleryModal.appendChild(imgModal);
+		});
 
 		// Adiciona no container principal
 		galleryContainer.appendChild(clone);
